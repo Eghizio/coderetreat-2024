769dc0bf6bf624ac3d0cac736d4c3566c911634a
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2024-11-16 10:15:58 +0100

    Failing Board generation

--- src/1-session/tests.py
+++ src/1-session/tests.py
@@ -4,7 +4,7 @@ from game import Game
 
 class TestGame(unittest.TestCase):
 
-    def test_should_generate_game_with_proper_dimensions(self):
+    def test_should_create_game_with_proper_dimensions(self):
         # Given
         width = 10
         height = 10
@@ -16,6 +16,21 @@ class TestGame(unittest.TestCase):
         self.assertEqual(game.width, width)
         self.assertEqual(game.height, height)
 
+    def test_should_generate_game_board_with_proper_dimensions(self):
+        # Given
+        width = 10
+        height = 10
+        fields_count = width * height
+        
+        # When
+        game = Game(width, height)
+
+        # Then
+        self.assertNotEqual(game.board, None)
+        self.assertEqual(isinstance(game.board, (dict)), True)
+        self.assertEqual(len(game.board), fields_count)
+
 
 if __name__ == "__main__":
     unittest.main()
+    # raise Exception("Not implemented")
