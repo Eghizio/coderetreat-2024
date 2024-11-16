7aa81218ba8f740c564f0e7f39c28151731c905a
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2024-11-16 16:22:52 +0100

    Azure OpenAI setup

--- src/5-session/Game.ts
+++ src/5-session/Game.ts
@@ -1,3 +1,8 @@
+import { AzureOpenAI } from "openai";
+import { AzureKeyCredential } from "@azure/core-auth";
+import dotenv from "dotenv";
+dotenv.config();
+
 // Game -> Set<Cell<x, y>>
 
 /*
@@ -6,6 +11,33 @@
   neighbours > 3      = overpopulation (dies)
   neighbours == 3     = born
 */
+
+const env = (value: string | undefined) => {
+  if (!value) throw new Error("dupa");
+  return value;
+};
+
+export class GPT {
+  private client: AzureOpenAI;
+
+  constructor() {
+    this.client = new AzureOpenAI({
+      apiKey: new AzureKeyCredential(env(process.env["API_KEY"])).key,
+      endpoint: env(process.env["API_ENDPOINT"]),
+      deployment: "gpt-4o-mini",
+      apiVersion: "2024-08-01-preview",
+    });
+  }
+
+  async prompt(message: string) {
+    const chatCompletion = await this.client.chat.completions.create({
+      messages: [{ role: "user", content: message }],
+      model: "",
+    });
+
+    return chatCompletion.choices[0].message?.content;
+  }
+}
 
 export class Cell {
   public x: number;
